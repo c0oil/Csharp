725d08c574691fc8a0a58784da0190b77e6b2f25
c0oil <serggvozd@yandex.ru>
2013-03-27 00:28:15 +0300

    begin

---
+++ cosii7/cosii5/DigitalSignalProcessor.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Point = System.Drawing.Point;
+
+namespace cosii5
+{
+    public class DigitalSignalProcessor
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        private const int BytePerPixel = 3;
+        private WriteableBitmap writeableBitmap;
+        
+
+        /*private byte[] GammaCorrection(byte[] inputPixels, int width, int height, int gmin, int gmax)
+        {
+            var outputPixels = new byte[inputPixels.Length];
+
+            int fmax = 0, fmin = 0;
+            FindF(inputPixels, ref fmin, ref fmax);
+
+            int fr = fmax - fmin, gr = gmax - gmin;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    byte r = inputPixels[BytePerPixel * (width * y + x) + 2],
+                         g = inputPixels[BytePerPixel * (width * y + x) + 1],
+                         b = inputPixels[BytePerPixel * (width * y + x) + 0];
+
+                    outputPixels[BytePerPixel * (width * y + x) + 2] = (byte)((r - fmin) * gr / fr + gmin);
+                    outputPixels[BytePerPixel * (width * y + x) + 1] = (byte)((g - fmin) * gr / fr + gmin);
+                    outputPixels[BytePerPixel * (width * y + x) + 0] = (byte)((b - fmin) * gr / fr + gmin);
+                }
+            }
+            return outputPixels;
+        }
+
+        private void FindF(byte[] inputBytes, ref int fmin, ref int fmax)
+        {
+            fmin = 255;
+            fmax = 0;
+            for (int i = 0; i < inputBytes.Length - 2; i += BytePerPixel)
+            {
+                byte r = inputBytes[i + 2],
+                     g = inputBytes[i + 1],
+                     b = inputBytes[i + 0];
+
+                int f = (int)(0.3 * r + 0.59 * g + 0.11 * b);
+                if (fmin > f) fmin = f;
+                if (fmax < f) fmax = f;
+            }
+        }*/
+
+        
+
+        public BitmapSource DetectImages(BitmapSource input, int clusters)
+        {
+            byte[] inputPixels = GetBytes(input);
+
+            return GetBitmap(inputPixels);
+        }
+
+        private static BitmapSource TransformBgr24(BitmapSource input)
+        {
+            var formatedBitmapSource = new FormatConvertedBitmap();
+            formatedBitmapSource.BeginInit();
+            formatedBitmapSource.Source = input;
+            formatedBitmapSource.DestinationPalette = input.Palette;
+            formatedBitmapSource.DestinationFormat = PixelFormats.Rgb24;
+            formatedBitmapSource.EndInit();
+            return formatedBitmapSource;
+        }
+
+        private byte[] GetBytes(BitmapSource imageSource)
+        {
+            writeableBitmap = new WriteableBitmap(imageSource);
+            var buffer = new byte[imageSource.PixelHeight * imageSource.PixelWidth * BytePerPixel];
+            writeableBitmap.CopyPixels(buffer, imageSource.PixelWidth * BytePerPixel, 0);
+            return buffer;
+        }
+
+        private BitmapSource GetBitmap(byte[] buffer)
+        {
+            writeableBitmap.WritePixels(new Int32Rect(0, 0, writeableBitmap.PixelWidth, writeableBitmap.PixelHeight), 
+                                        buffer, writeableBitmap.PixelWidth * BytePerPixel, 0);
+            return writeableBitmap;
+        }
+    }
+}
---
+++ cosii7/cosii5/ImageRedactorModel.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Input;
+using Microsoft.Win32;
+using cosii5.MVVMUtility;
+
+namespace cosii5
+{
+    public class ImageRedactorModel : ViewModelBase
+    {
+        private const string SourcePath = @"D:\www_goodfon_ru\12828-1366x768.jpg";
+        public DigitalSignalProcessor Dsp { get; set; }
+        public RelayCommand StartCommand { get; private set; }
+        public RelayCommand FullScreenCommand { get; private set; }
+
+        #region Bindble properties
+        private BitmapSource sourceImage;
+        public BitmapSource SourceImage 
+        {
+            get { return sourceImage; }
+            set
+            {
+                sourceImage = value;
+                OnPropertyChanged("SourceImage");
+            }
+        }
+
+        private BitmapSource grayscaleImage;
+        public BitmapSource GrayscaleImage
+        {
+            get { return grayscaleImage; }
+            set
+            {
+                grayscaleImage = value;
+                OnPropertyChanged("GrayscaleImage");
+            }
+        }
+
+        private BitmapSource binarImage;
+        public BitmapSource BinarImage
+        {
+            get { return binarImage; }
+            set
+            {
+                binarImage = value;
+                OnPropertyChanged("BinarImage");
+            }
+        }
+
+        private BitmapSource targetImage;
+        public BitmapSource TargetImage
+        {
+            get { return targetImage; }
+            set
+            {
+                targetImage = value;
+                OnPropertyChanged("TargetImage");
+            }
+        }
+
+        public string BinarScore { get; set; }
+        public string Clusters { get; set; }
+        #endregion
+
+        public ImageRedactorModel()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            SourceImage = new BitmapImage(new Uri(SourcePath));
+            StartCommand = new RelayCommand(OnStart) { IsEnabled = true };
+            Dsp = new DigitalSignalProcessor();
+            BinarScore = "150";
+            Clusters = "4";
+        }
+
+        private void OnStart()
+        {
+            Dsp.Width = SourceImage.PixelWidth;
+            Dsp.Height = SourceImage.PixelHeight;
+        }
+
+        public void OpenExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            var ofd = new OpenFileDialog { InitialDirectory = @"D:\www_goodfon_ru" };
+	        if (ofd.ShowDialog() == true)
+	        {
+                SourceImage = new BitmapImage(new Uri(ofd.FileName));
+	        }
+        }
+
+        public void CanOpen(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        public static void SaveImageToFile(string filePath, BitmapSource image)
+        {
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                BitmapEncoder encoder = new JpegBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(image));
+                encoder.Save(fileStream);
+            }
+        }
+    }
+}
