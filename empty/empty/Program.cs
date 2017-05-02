c44b4a92b21d225547bd794e78eaf18d9c60fa3d
ssa <serggvozd@yandex.ru>
2017-05-02 19:30:58 +0300

    Exposure import was added

--- empty/empty/Program.cs
+++ empty/empty/Program.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Xml;
 using System.Xml.Serialization;
+using RaObjects.Objects;
 
 namespace empty
 {
@@ -18,7 +20,15 @@ namespace empty
         /// </param>
         public static void Main(string[] args)
         {
-            new RaXmlImporterExample().Import();
+            using (XmlReader reader = new XmlTextReader("opera.xml") { WhitespaceHandling = WhitespaceHandling.None })
+            {
+                while (reader.Name != "opera")
+                {
+                    reader.Read();
+                }
+                RiskAggregationData result = RaXmlImportGenerator.ImportRiskAggregationData(reader, null);
+            }
+            //new RaXmlImporterExample().Import();
             /*Opera newPerson = null;
             using (FileStream fs = new FileStream("opera.xml", FileMode.OpenOrCreate))
             {
