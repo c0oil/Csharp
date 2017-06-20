28cf0b6da8833c3ff9b91a72ee5971cccb4cbb45
ssa <serggvozd@yandex.ru>
2017-06-21 00:03:43 +0300

    evroopt parce

---
+++ HtmlParcer/HtmlParcer/HtmlParcer/HtmlHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace HtmlParcer
+{
+    public static class HtmlNodeHelper
+    {
+        public static HtmlNode GetSecondColumnByFirst(this HtmlNode node, string value)
+        {
+            return node.ChildNodes.FirstOrDefault(x => x.ChildNodes[0].InnerText.StartsWith(value))?.ChildNodes[1];
+        }
+
+        public static HtmlNode GetChildByClassAttribute(this HtmlNode node, string value)
+        {
+            if (node == null)
+                return null;
+
+            var result = node.ChildNodes.FirstOrDefault(x => HaveAttibute(x, "class", value));
+            if (result == null)
+            {
+                
+            }
+            return result;
+        }
+
+        public static HtmlNode GetChildByClassAttributeStartWith(this HtmlNode node, string value)
+        {
+            return node.ChildNodes.First(x => HaveAttibuteStartWith(x, "class", value));
+        }
+
+        private static bool HaveAttibute(HtmlNode node, string name, string value)
+        {
+            var classAttr = node.Attributes[name];
+            return classAttr != null && classAttr.Value == value;
+        }
+
+        private static bool HaveAttibuteStartWith(HtmlNode node, string name, string value)
+        {
+            var classAttr = node.Attributes[name];
+            return classAttr != null && classAttr.Value.StartsWith(value);
+        }
+
+        public static bool IsStartWithClassAttribute(this HtmlNode node, string value)
+        {
+            return node.Attributes["class"].Value.StartsWith(value);
+        }
+
+    }
+}
---
+++ HtmlParcer/HtmlParcer/HtmlParcer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using HtmlAgilityPack;
+using System.Xml.Serialization;
+
+namespace HtmlParcer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parcer = new WebParcer();
+
+            // Колбасы, сардельки, сосиски
+            //parcer.ParcePage("Колбасы вареные");
+            //parcer.ParcePage("Колбасы сырокопченые, копченые");
+            //parcer.ParcePage("Колбасы из мясопродуктов, паштеты");
+            //parcer.ParcePage("Сардельки");
+            //parcer.ParcePage("Сосиски");
+
+            // Говядина
+            //parcer.ParcePage("Копчености из говядины");
+
+            // Птица
+            //parcer.ParcePage("Копчености из мяса птицы");
+
+            //Свинина
+            //parcer.ParcePage("Копчености из свинины");
+            parcer.ParcePage("Сыровяленые, соленые изделия из свинины");
+
+        }
+    }
+}
